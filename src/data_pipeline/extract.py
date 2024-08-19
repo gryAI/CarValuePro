a125100d0b40707ec38bddede1c7ea7e0bdb6bbf
Lumine_ <grace.estrada@ftwfoundation.org>
2024-08-19 19:44:09 +0800

    [feat] data_pipeline/extract: configured selenium web driver

--- src/data_pipeline/extract.py
+++ src/data_pipeline/extract.py
@@ -1,10 +1,18 @@
+# Import Libraries
+import time
+
+import pandas as pd
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-from bs4 import BeautifulSoup
 
-import pandas as pd
-import time
+
+
+
+# Configure Selenium Web Driver
+driver = webdriver.Chrome("C:\Drivers\chromedriver.exe")
+driver.get("https://philkotse.com/used-cars-for-sale")
